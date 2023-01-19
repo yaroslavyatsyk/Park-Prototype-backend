ef2c4e18516d38beb8ca0dba669580908c664fe1
Yaroslav9728 <yaroslav9728@gmail.com>
2023-01-18 22:59:15 -0500

    Updated location model, added route for location

--- backend/models/location.js
+++ backend/models/location.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const locationSchema = new Schema({
-
-    longitude: Number,
-    latitude: Number
+    name: {
+        type: String,
+        required : true
+    },
+    coordinates: {
+        type: [Number],
+        index: "2dsphere"
+    }
 
 })
 
---
+++ backend/routes/locationRoutes.js
@@ -0,0 +1,108 @@
+const express = require('express')
+const route = express.Router()
+const loc = require('../models/location')
+
+route.post('/location', async(req, res) => {
+    // Validate request
+    const newLocation = req.body;
+    if(JSON.stringify(newLocation) == null || JSON.stringify(newLocation) == '{}') {
+        return res.status(400).send({
+            message: "Employee content can not be empty"
+        });
+    }
+    else {
+
+    try {
+        const location = new emp(newLocation)
+        await location.save()
+        res.status(201).send(location)
+    }
+    catch(error) {
+        res.status(500).send(error)
+    }
+}
+});
+
+//http://mongoosejs.com/docs/api.html#find_find
+route.get('/locations', async(req, res) => {
+    try {
+        const locations = await loc.find({})
+        res.status(200).send(locations)
+    }
+    catch(error) {
+        res.status(500).send(error)
+    }
+});
+
+
+//http://mongoosejs.com/docs/api.html#findbyid_findById
+route.get('/locations/:id', async(req, res) => {
+    // Validate request
+    let id = req.params.id
+    if(JSON.stringify(id) == null || JSON.stringify(id) == '{}') {
+        return res.status(400).send({
+            message: "Location content can not be empty"
+        });
+    }
+    else {
+    
+    try {
+        const location = await emp.findById(req.params.id)
+        res.status(200).send(location)
+    }
+    catch(error) {
+        res.status(500).send(error)
+    }
+}
+});
+
+//TODO - Update a Note with noteId
+//http://mongoosejs.com/docs/api.html#findbyidandupdate_findByIdAndUpdate
+route.put('/locations/id', async(req, res) => {
+    // Validate request
+    let id = req.params.id
+    if(JSON.stringify(id) == null || JSON.stringify(id) == '{}') {
+        return res.status(400).send({
+            message: "Location's content can not be empty"
+        });
+    }
+    else {
+    try {
+        console.log(req.body)
+        const updatedLocation = await emp.findByIdAndUpdate(req.params.id, req.body)
+        //console.log(updatedBook)
+        await updatedLocation.save()
+        res.status(202).send(updatedLocation)
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    }
+});
+
+//TODO - Delete a Note with noteId
+//http://mongoosejs.com/docs/api.html#findbyidandremove_findByIdAndRemove
+route.delete('/locations/:id', async (req, res) => {
+    // Validate request
+    let id = req.params.eid
+    if(JSON.stringify(id) == null || JSON.stringify(id) == '{}') {
+        return res.status(400).send({
+            message: "Location's content can not be empty"
+        });
+    }
+    //TODO - Write your code here to delete the note using
+    else {
+    try {
+        const location = await loc.findByIdAndDelete(req.params.eid)
+    
+        if (!location) { 
+            res.status(404).send("No item found")
+        }
+        res.status(204).send(location)
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    }
+});
+module.exports = route
+
+
