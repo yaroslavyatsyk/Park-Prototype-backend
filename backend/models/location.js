c6afd371fcd4ca4de131153cae45a07523916fd1
Yaroslav9728 <yaroslav9728@gmail.com>
2023-01-19 10:07:10 -0500

    fixed location.js model

--- backend/models/location.js
+++ backend/models/location.js
@@ -3,14 +3,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const locationSchema = new Schema({
-    name: {
-        type: String,
-        required : true
-    },
-    coordinates: {
-        type: [Number],
-        index: "2dsphere"
-    }
+    latitude : Number,
+    longitude: Number
 
 })
 
