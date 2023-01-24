4c2e8a6301c8de86f06423730bf3ebb875ea7ab6
Yaroslav9728 <yaroslav9728@gmail.com>
2023-01-23 20:40:57 -0500

    Initial commit of the "server.js" file

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bookingRouter = require('./routes/bookingRoutes')
+const locationRouter = require('./routes/locationRoutes')
+
+const app = express()
+app.use(express.json())
+
+app.use('/api',bookingRouter)
+app.use('/api/emp',locationRouter)
+
+
+mongoose.Promise = global.Promise
+
+mongoose.connect("mongodb+srv://yaroslav9728:Education2022@cluster0.elr77qb.mongodb.net/ass1db?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connected to the database mongoDB Atlas Server");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome to Note taking application - Week06 Exercise</h1>");
+});
+
+
+app.listen(3035, () => {
+    console.log(`Server is listening on port http://localhost:${3035}`);
+});
